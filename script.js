4cf0330313cc037ea1af4af04e0e89ad44ad3e4b
Ngocthai2703web1s <159770013+Ngocthai2703web1s@users.noreply.github.com>
2025-05-15 09:07:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ const categoryLabels = {
 };
 
 const items = [
-  { name: 'Án Mạng Trong Vô Thức - Melinda Leigh', category: 'TrinhTham', url: 'https://drive.google.com/file/d/1-FZlXmOZBcNKDmGDjThqt6GVwk6ruBee/view?usp=drive_link' },
-  { name: 'Hồ Sơ Tâm Lý Tội Phạm Tập 1 - Cương Tuyết Ấn', category: 'TrinhTham', url: 'https://drive.google.com/file/d/1-fHwKS_F1qbK2m_LclwjE3uRaEOo2krN/view?usp=drive_link' },
-  { name: 'Hồ Sơ Tâm Lý Tội Phạm Tập 2 - Cương Tuyết Ấn', category: 'TrinhTham', url: 'https://drive.google.com/file/d/1-MZmm5tXlHtT7c1f0CUxSjjSQKQ27Tn-/view?usp=drive_link' },
-  { name: 'Trộm Mệnh Đôi Cá Âm Dương Tập 1 – Du Hồn Nhập Mộng', category: 'LinhDi', url: 'https://drive.google.com/file/d/103pJxPUnmsSg10biQc_VjxITxw2JmZ3p/view?usp=drive_link' },
-  { name: 'Trộm Mệnh Lư Hương Ngư VănTập 2 – Chu Sa Chỉ Lối', category: 'LinhDi', url: 'https://drive.google.com/file/d/10XHuhPYu6CT3kt7l0yVC40CKuM5Fj_49/view?usp=drive_link' }
+  { name: 'Án Mạng Trong Vô Thức - Melinda Leigh', category: 'TrinhTham', url: 'https://drive.google.com/file/d/1-FZlXmOZBcNKDmGDjThqt6GVwk6ruBee/view?usp=drive_link', imageUrl: 'https://ebookvie.com/wp-content/uploads/2025/04/cover-62.jpg' },
+  { name: 'Hồ Sơ Tâm Lý Tội Phạm Tập 1 - Cương Tuyết Ấn', category: 'TrinhTham', url: 'https://drive.google.com/file/d/1-fHwKS_F1qbK2m_LclwjE3uRaEOo2krN/view?usp=drive_link', imageUrl: 'https://ebookvie.com/wp-content/uploads/2023/12/Ho-So-Tam-Ly-Pham-Toi-Cuong-Tuyet-An.jpg' },
+  { name: 'Hồ Sơ Tâm Lý Tội Phạm Tập 2 - Cương Tuyết Ấn', category: 'TrinhTham', url: 'https://drive.google.com/file/d/1-MZmm5tXlHtT7c1f0CUxSjjSQKQ27Tn-/view?usp=drive_link', imageUrl: 'https://ebookvie.com/wp-content/uploads/2025/04/cover-59.jpg' },
+  { name: 'Trộm Mệnh Đôi Cá Âm Dương Tập 1 – Du Hồn Nhập Mộng', category: 'LinhDi', url: 'https://drive.google.com/file/d/103pJxPUnmsSg10biQc_VjxITxw2JmZ3p/view?usp=drive_link', imageUrl: 'https://ebookvie.com/wp-content/uploads/2025/04/doi-ca-am-duong-du-hon-nhap-mong-nhat-mai-dong-tien-han-vu-phi-dich.jpg' },
+  { name: 'Trộm Mệnh Lư Hương Ngư VănTập 2 – Chu Sa Chỉ Lối', category: 'LinhDi', url: 'https://drive.google.com/file/d/10XHuhPYu6CT3kt7l0yVC40CKuM5Fj_49/view?usp=drive_link', imageUrl: 'https://ebookvie.com/wp-content/uploads/2025/04/trom-menh-nhat-mai-dong-tien.jpg' }
 ];
 
 function renderList(listItems) {
@@ -33,6 +33,7 @@ function renderList(listItems) {
     .forEach(item => {
       const li = document.createElement('li');
       li.innerHTML = `
+        <img src="${item.imageUrl}" alt="${item.name}" class="item-image">
         <a href="${item.url}" class="item-name" target="_blank" rel="noopener noreferrer">${item.name}</a>
         <span class="category ${item.category}">${categoryLabels[item.category]}</span>
       `;
